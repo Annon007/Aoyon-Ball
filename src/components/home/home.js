617b8fd91567054841c017b19973efa95336c26b
Aoyon123 <49103044+Aoyon123@users.noreply.github.com>
2022-02-16 02:48:20 +0600

    images

---
+++ src/components/home/home.js
@@ -0,0 +1,10 @@
+import NavBar from "../../core/navbar/navbar";
+import styles from "./home.module.css";
+
+const Home = () =>{
+    return<div>
+        <NavBar/>
+    </div>
+};
+
+export default Home;
---
+++ src/core/navbar/navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./navbar.module.css";
+
+const NavBar = props => {
+    return <div>
+        HI
+    </div>
+};
+
+export default NavBar;
